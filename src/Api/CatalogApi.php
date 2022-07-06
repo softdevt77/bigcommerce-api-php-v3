377e572a5b81bb15669432098dd33195c5900ca9
softdevt77 <stevengillespie774@gmail.com>
2022-07-06 14:37:55 +0300

    BCP20-1415 Add category batch endpoint support

--- src/Api/CatalogApi.php
+++ src/Api/CatalogApi.php
@@ -6825,6 +6825,89 @@ class CatalogApi
             throw $e;
         }
     }
+
+    /**
+     * Operation getCategoriesBatch
+     *
+     *
+     *
+     * @param array $params = []
+     * @return \BigCommerce\Api\v3\Model\CategoryCollectionResponse
+     * @throws \BigCommerce\Api\v3\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     */
+    public function getCategoriesBatch(array $params = [])
+    {
+        list($response) = $this->getCategoriesBatchWithHttpInfo($params);
+
+        return $response;
+    }
+
+    /**
+     * Operation getCategoriesBatchWithHttpInfo
+     *
+     * @see self::getCategoriesBatch()
+     * @param array $params = []
+     * @throws \BigCommerce\Api\v3\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return array of \BigCommerce\Api\v3\Model\CategoryCollectionResponse, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function getCategoriesBatchWithHttpInfo(array $params = [])
+    {
+        // parse inputs
+        $resourcePath = "/catalog/trees/categories";
+        $httpBody = '';
+        $queryParams = [];
+        $headerParams = [];
+        $formParams = [];
+        $_header_accept = $this->apiClient->selectHeaderAccept(['application/json']);
+        if (!is_null($_header_accept)) {
+            $headerParams['Accept'] = $_header_accept;
+        }
+        $headerParams['Content-Type'] = $this->apiClient->selectHeaderContentType(['application/json']);
+
+        // query params
+        foreach ( $params as $key => $param ) {
+            $queryParams[ $key ] = $this->apiClient->getSerializer()->toQueryValue( $param );
+        }
+
+        // default format to json
+        $resourcePath = str_replace("{format}", "json", $resourcePath);
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            $httpBody = $_tempBody; // $_tempBody is the method argument, if present
+        } elseif (count($formParams) > 0) {
+            $httpBody = $formParams; // for HTTP post (form)
+        }
+
+        // make the API Call
+        try {
+            list($response, $statusCode, $httpHeader) = $this->apiClient->callApi(
+                $resourcePath,
+                'GET',
+                $queryParams,
+                $httpBody,
+                $headerParams,
+                '\BigCommerce\Api\v3\Model\CategoryCollectionResponse',
+                '/catalog/trees/categories'
+            );
+            return [$this->apiClient->getSerializer()->deserialize($response, '\BigCommerce\Api\v3\Model\CategoryCollectionResponse', $httpHeader), $statusCode, $httpHeader];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+
+                case 200:
+                    $data = $this->apiClient->getSerializer()->deserialize($e->getResponseBody(), '\BigCommerce\Api\v3\Model\CategoryCollectionResponse', $e->getResponseHeaders());
+                    $e->setResponseObject($data);
+                    break;
+
+            }
+
+            throw $e;
+        }
+    }
+
     /**
      * Operation getCategoryTree
      *
